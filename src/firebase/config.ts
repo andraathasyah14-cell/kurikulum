7bf52263d432717d0dea59b922e383621cea4e14
andra athasyah <andraathasyah11@gmail.com>
2026-03-10 04:33:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7519382852-b6a2e",
+  "appId": "1:469838096174:web:8c6a087fef7ae6bd5d3cbf",
+  "apiKey": "AIzaSyC6GrTIJdrpFGA5u4roRQ_D0CMAVkGlnts",
+  "authDomain": "studio-7519382852-b6a2e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "469838096174"
+};
